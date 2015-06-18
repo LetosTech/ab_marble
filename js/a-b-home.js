db43578d5e18d72150578fe1eea045e2e52c09e2
Diogo Silva <diogosilva@Diogos-MacBook-Pro.local>
2015-06-17 23:36:48 -0400

    AbMarble
    html and css Training.

---
+++ js/a-b-home.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+  
+	$("a.fancybox").fancybox();
+	
+	$(".contact_form").validate();
+
+});
---
+++ js/a-b.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+  
+  $(".sidebar").height($(".contactus").height() + 170);
+	
+	
+
+});
